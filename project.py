6897b493299c7a0d75603d30dd801d6aca6093a1
Mohanraj4527 <mohanmech13899@gmail.com>
2025-03-04 10:14:36 +0530

    Update project.php

--- project.py
+++ project.py
@@ -1,31 +1,17 @@
-#!C:\Program Files\Python313.exe
-import cgi
-import mysql.connector
-
-
-print("Content-Type: text/html\n\n")
-print("<html>")
-print("<body>")
-print("<h1>Thank You for ordering</h1>")
-
-
-form=cgi.fieldstorage()
-name=form.getvalue("name")
-product=form.getvalue("product")
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="orderform"
-)
-mycursor=mydb.cursor()
-sql="INSERT INTO details(name,product) values(%s,%s)"
-val=(name,product)
-mycursor.execute(sql,val)
-mydb.commit()
-
-print("<h1>",name,product,"</h1>")
-
-print("</body>")
-print("</html>")
+<?php
+    $Name    = $_POST['Name'];
+    $Product = $_POST['Product'];
+    
+    //Database connection
+    $conn = new mysqli('localhost','root','','orderform');
+    if($conn->connect_error){
+        die('connection Failed : ' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into orderdetails(Name,Product)
+        values(?,?)");
+        $stmt->bind_param("ss",$Name,$Product);
+        $stmt->execute();
+        echo "Thank You For Ordering...";
+        $stmt->Close();
+        $conn->Close();
+    }
